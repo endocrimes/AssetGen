25ce3867cd0ec204cbab9ad67a988923c4acb17e
Daniel Tomlinson <Dan@Tomlinson.io>
2015-12-08 19:50:34 +0000

    Inital Commit

---
+++ Tests/Fixtures/TestApplication1/TestApplication1/ViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ViewController.swift
+//  TestApplication1
+//
+//  Created by Daniel Tomlinson on 08/12/2015.
+//  Copyright © 2015 Rocket Apps Limited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+}
